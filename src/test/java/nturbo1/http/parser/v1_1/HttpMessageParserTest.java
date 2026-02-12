f5d8ec13c28a9a4664e343d9fbd5414b44fb1475
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2026-02-12 18:42:47 +0500

    Add parameterized unit tests for parsing an http version in an http message req/res line

--- src/test/java/nturbo1/http/parser/v1_1/HttpMessageParserTest.java
+++ src/test/java/nturbo1/http/parser/v1_1/HttpMessageParserTest.java
@@ -1,12 +1,12 @@
 package nturbo1.http.parser.v1_1;
 
-import nturbo1.http.HttpMethod;
 import nturbo1.exceptions.parser.HttpMessageParseException;
-    
+import nturbo1.http.HttpMethod;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.assertj.core.api.Assertions;
 
 class HttpMessageParserTest
 {
@@ -23,5 +23,19 @@ class HttpMessageParserTest
     {
         String unknownHttpMethod = "asdfasdf";
         Assertions.assertThatThrownBy(() -> HttpMessageParser.parseHttpMethod(unknownHttpMethod));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "HTTP/0.9, 0.9",
+            "HTTP/1.0, 1.0",
+            "HTTP/1.1, 1.1",
+            "HTTP/2.0, 2.0",
+            "HTTP/3.0, 3.0"
+    })
+    void returnValidHttpVersion(String versionTxt, float expectedVersion) throws HttpMessageParseException
+    {
+        float parsedVersion = HttpMessageParser.parseHttpVersion(versionTxt);
+        Assertions.assertThat(parsedVersion).isEqualTo(expectedVersion);
     }
 }
